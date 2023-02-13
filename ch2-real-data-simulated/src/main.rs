596e82435733cd565f2cc2c2e2f562144e1843cf
Jermaine Allgood <jermaine.allgood@windriver.com>
2023-02-13 12:16:03 -0800

    added output

--- ch2-real-data-simulated/src/main.rs
+++ ch2-real-data-simulated/src/main.rs
@@ -22,6 +22,29 @@ fn main() {
         .finish();
 
     dbg!(df_hw_biased.as_ref().expect("No data available").with_row_count("Id", None));
+
+    /*
+    Finished dev [unoptimized + debuginfo] target(s) in 1.09s
+     Running `target/debug/ch2-real-data-simulated`
+[src/main.rs:24] df_hw_biased.as_ref().expect("No data available").with_row_count("Id", None) = Ok(
+    shape: (500, 5)
+    ┌─────┬────────┬────────┬────────┬───────┐
+    │ Id  ┆ Gender ┆ Height ┆ Weight ┆ Index │
+    │ --- ┆ ---    ┆ ---    ┆ ---    ┆ ---   │
+    │ u32 ┆ str    ┆ i64    ┆ i64    ┆ i64   │
+    ╞═════╪════════╪════════╪════════╪═══════╡
+    │ 0   ┆ Male   ┆ 174    ┆ 96     ┆ 4     │
+    │ 1   ┆ Male   ┆ 189    ┆ 87     ┆ 2     │
+    │ 2   ┆ Female ┆ 185    ┆ 110    ┆ 4     │
+    │ 3   ┆ Female ┆ 195    ┆ 104    ┆ 3     │
+    │ ... ┆ ...    ┆ ...    ┆ ...    ┆ ...   │
+    │ 496 ┆ Female ┆ 184    ┆ 121    ┆ 4     │
+    │ 497 ┆ Female ┆ 141    ┆ 136    ┆ 5     │
+    │ 498 ┆ Male   ┆ 150    ┆ 95     ┆ 5     │
+    │ 499 ┆ Male   ┆ 173    ┆ 131    ┆ 5     │
+    └─────┴────────┴────────┴────────┴───────┘,
+)
+    */
     
 
    
