2b210de2b0612e30d9c83a66c8592fb67811515d
Jermaine Allgood <jermaine.allgood@windriver.com>
2023-02-13 16:27:22 -0800

    just simple dataframe stats

--- ch2-real-data-simulated/src/main.rs
+++ ch2-real-data-simulated/src/main.rs
@@ -1,14 +1,4 @@
-// extern crate csv;
-// extern crate ndarray;
-// extern crate ndarray_csv;
-
-// use csv::ReaderBuilder;
-// use std::error::Error;
-// use std::fs::File;
-// use ndarray::{s, Array2};
-// use ndarray_csv::Array2Reader;
-
-use polars::prelude::CsvReader;
+use polars::{prelude::CsvReader};
 use polars_core::prelude::*;
 use polars_io::prelude::*;
 use polars::frame::DataFrame;
@@ -21,32 +11,80 @@ fn main() {
         .has_header(true)
         .finish();
 
-    dbg!(df_hw_biased.as_ref().expect("No data available").with_row_count("Id", None));
+println!("Standard Deviation {} ", df_hw_biased.as_ref().expect("cannnot").mean());
+println!("Mean {} ", df_hw_biased.as_ref().expect("No std found").std(1));
+println!("Min {} ", df_hw_biased.as_ref().expect("No minimum value found").min());
+println!("Max {} ", df_hw_biased.as_ref().expect("No maximum value found").max());
+println!("Median {} ", df_hw_biased.as_ref().expect("No median value found").median());
+println!("Head {} ", df_hw_biased.as_ref().expect("No head values found").head(Some(5)));
+println!("Number of Rows -  {} ", df_hw_biased.as_ref().expect("No rows found").height());
+println!("Number of Columns -  {} ", df_hw_biased.as_ref().expect("No maximum value found").width());
 
-    /*
-    Finished dev [unoptimized + debuginfo] target(s) in 1.09s
+
+
+/*
+cargo run
+    Finished dev [unoptimized + debuginfo] target(s) in 0.15s
      Running `target/debug/ch2-real-data-simulated`
-[src/main.rs:24] df_hw_biased.as_ref().expect("No data available").with_row_count("Id", None) = Ok(
-    shape: (500, 5)
-    ┌─────┬────────┬────────┬────────┬───────┐
-    │ Id  ┆ Gender ┆ Height ┆ Weight ┆ Index │
-    │ --- ┆ ---    ┆ ---    ┆ ---    ┆ ---   │
-    │ u32 ┆ str    ┆ i64    ┆ i64    ┆ i64   │
-    ╞═════╪════════╪════════╪════════╪═══════╡
-    │ 0   ┆ Male   ┆ 174    ┆ 96     ┆ 4     │
-    │ 1   ┆ Male   ┆ 189    ┆ 87     ┆ 2     │
-    │ 2   ┆ Female ┆ 185    ┆ 110    ┆ 4     │
-    │ 3   ┆ Female ┆ 195    ┆ 104    ┆ 3     │
-    │ ... ┆ ...    ┆ ...    ┆ ...    ┆ ...   │
-    │ 496 ┆ Female ┆ 184    ┆ 121    ┆ 4     │
-    │ 497 ┆ Female ┆ 141    ┆ 136    ┆ 5     │
-    │ 498 ┆ Male   ┆ 150    ┆ 95     ┆ 5     │
-    │ 499 ┆ Male   ┆ 173    ┆ 131    ┆ 5     │
-    └─────┴────────┴────────┴────────┴───────┘,
-)
-    */
+Standard Deviation shape: (1, 4)
+┌────────┬─────────┬────────┬───────┐
+│ Gender ┆ Height  ┆ Weight ┆ Index │
+│ ---    ┆ ---     ┆ ---    ┆ ---   │
+│ str    ┆ f64     ┆ f64    ┆ f64   │
+╞════════╪═════════╪════════╪═══════╡
+│ null   ┆ 169.944 ┆ 106.0  ┆ 3.748 │
+└────────┴─────────┴────────┴───────┘
+Mean shape: (1, 4)
+┌────────┬───────────┬───────────┬──────────┐
+│ Gender ┆ Height    ┆ Weight    ┆ Index    │
+│ ---    ┆ ---       ┆ ---       ┆ ---      │
+│ str    ┆ f64       ┆ f64       ┆ f64      │
+╞════════╪═══════════╪═══════════╪══════════╡
+│ null   ┆ 16.375261 ┆ 32.382607 ┆ 1.355053 │
+└────────┴───────────┴───────────┴──────────┘
+Min shape: (1, 4)
+┌────────┬────────┬────────┬───────┐
+│ Gender ┆ Height ┆ Weight ┆ Index │
+│ ---    ┆ ---    ┆ ---    ┆ ---   │
+│ str    ┆ i64    ┆ i64    ┆ i64   │
+╞════════╪════════╪════════╪═══════╡
+│ Female ┆ 140    ┆ 50     ┆ 0     │
+└────────┴────────┴────────┴───────┘
+Max shape: (1, 4)
+┌────────┬────────┬────────┬───────┐
+│ Gender ┆ Height ┆ Weight ┆ Index │
+│ ---    ┆ ---    ┆ ---    ┆ ---   │
+│ str    ┆ i64    ┆ i64    ┆ i64   │
+╞════════╪════════╪════════╪═══════╡
+│ Male   ┆ 199    ┆ 160    ┆ 5     │
+└────────┴────────┴────────┴───────┘
+Median shape: (1, 4)
+┌────────┬────────┬────────┬───────┐
+│ Gender ┆ Height ┆ Weight ┆ Index │
+│ ---    ┆ ---    ┆ ---    ┆ ---   │
+│ str    ┆ f64    ┆ f64    ┆ f64   │
+╞════════╪════════╪════════╪═══════╡
+│ null   ┆ 170.5  ┆ 106.0  ┆ 4.0   │
+└────────┴────────┴────────┴───────┘
+Head shape: (5, 4)
+┌────────┬────────┬────────┬───────┐
+│ Gender ┆ Height ┆ Weight ┆ Index │
+│ ---    ┆ ---    ┆ ---    ┆ ---   │
+│ str    ┆ i64    ┆ i64    ┆ i64   │
+╞════════╪════════╪════════╪═══════╡
+│ Male   ┆ 174    ┆ 96     ┆ 4     │
+│ Male   ┆ 189    ┆ 87     ┆ 2     │
+│ Female ┆ 185    ┆ 110    ┆ 4     │
+│ Female ┆ 195    ┆ 104    ┆ 3     │
+│ Male   ┆ 149    ┆ 61     ┆ 3     │
+└────────┴────────┴────────┴───────┘
+Number of Rows -  500
+Number of Columns -  4
+*/
+
+
+
     
 
-   
     
 }
